a991893972a158d96c0455625e3e58600f2355da
Muhamad Ariel Septiadi <arielseptiadi3@gmail.com>
2024-12-23 13:00:32 +0700

    Update - Perbaikan Tombol Delete Layanan Yang Mengarah Ke Route yang salah

--- routes/web.php
+++ routes/web.php
@@ -24,7 +24,6 @@ Route::post('/reset-password', [PasswordResetController::class, 'resetPassword']
 Route::get('/auth-google-redirect', [AuthController::class, 'google_redirect']);
 Route::get('/auth-google-callback', [AuthController::class, 'google_callback']);
 
-
 Route::group(['middleware' => ['auth', 'check_role:user']], function () {
     Route::get('/user/user', function () {
         return view('user/user');
@@ -36,7 +35,7 @@ Route::group(['middleware' => ['auth', 'check_role:user']], function () {
 
 Route::group(['middleware' => ['auth', 'check_role:admin']], function () {
     Route::get('/admin/dashboard', [DashboardController::class, 'index']);
-  
+
 
     // Route::get('/admin/services', );
     // Route::get('/admin/services/', [DashboardController::class, 'servicesindex'], [ServiceController::class, 'adminIndex'])->middleware('auth');
@@ -45,44 +44,37 @@ Route::group(['middleware' => ['auth', 'check_role:admin']], function () {
     Route::get('/admin/services/{service}/edit', [ServiceController::class, 'edit'])->name('admin.services.edit');
     Route::put('/admin/services/{service}', [ServiceController::class, 'update'])->name('admin.services.update');
     Route::delete('/admin/services/{service}', [ServiceController::class, 'destroy'])->name('admin.services.destroy');
-    // Rute untuk menampilkan daftar layanan
-Route::get('/services', [ServiceController::class, 'index'])->name('services.index');
 
+    // ADMIN
 
-// Rute untuk menampilkan form membuat layanan baru
-Route::get('/services/create', [ServiceController::class, 'create'])->name('services.create');
+    // Ganti route statis dengan method adminIndex di ServiceController
+    Route::get('/admin/services', [ServiceController::class, 'adminIndex'])->name('admin.services.index');
 
-// Rute untuk menyimpan layanan baru
-Route::post('/services', [ServiceController::class, 'store'])->name('services.store');
+    // Rute untuk mengambil data dari API
+    Route::post('admin/services/fetch', [ServiceController::class, 'fetchServices'])->name('admin.services.fetch');
+    // Operasi CRUD di admin tetap mengacu pada ServiceController
+    Route::get('/admin/services/create', [ServiceController::class, 'create'])->name('admin.services.create');
+    Route::post('/admin/services', [ServiceController::class, 'store'])->name('admin.services.store');
+    Route::get('/admin/services/{service}/edit', [ServiceController::class, 'edit'])->name('admin.services.edit');
+    Route::put('/admin/services/{service}', [ServiceController::class, 'update'])->name('admin.services.update');
+    Route::delete('/admin/services/{service}', [ServiceController::class, 'destroy'])->name('admin.services.destroy');
 
-// Rute untuk menampilkan form edit layanan
-Route::get('/services/{service}/edit', [ServiceController::class, 'edit'])->name('services.edit');
-
-// Rute untuk memperbarui layanan
-Route::put('/services/{service}', [ServiceController::class, 'update'])->name('services.update');
-
-// Rute untuk menghapus layanan
-Route::delete('/services/{service}', [ServiceController::class, 'destroy'])->name('services.destroy');
-
-// Rute CRUD untuk kategori
-Route::resource('categories', CategoryController::class);
-
-
-// ADMIN
-
-// Ganti route statis dengan method adminIndex di ServiceController
-Route::get('/admin/services', [ServiceController::class, 'adminIndex'])->name('admin.services.index');
-
-// Rute untuk mengambil data dari API
-Route::post('admin/services/fetch', [ServiceController::class, 'fetchServices'])->name('admin.services.fetch');
-// Operasi CRUD di admin tetap mengacu pada ServiceController
-Route::get('/admin/services/create', [ServiceController::class, 'create'])->name('admin.services.create');
-Route::post('/admin/services', [ServiceController::class, 'store'])->name('admin.services.store');
-Route::get('/admin/services/{service}/edit', [ServiceController::class, 'edit'])->name('admin.services.edit');
-Route::put('/admin/services/{service}', [ServiceController::class, 'update'])->name('admin.services.update');
-Route::delete('/admin/services/{service}', [ServiceController::class, 'destroy'])->name('admin.services.destroy');
-
-// tambahkan akses lain untuk admin
+    // tambahkan akses lain untuk admin
 });
 
 Route::get('/logout', [AuthController::class, 'logout']);
+
+// Rute untuk menampilkan daftar layanan
+Route::get('/services', [ServiceController::class, 'index'])->name('services.index');
+// Rute untuk menampilkan form membuat layanan baru
+Route::get('/services/create', [ServiceController::class, 'create'])->name('services.create');
+// Rute untuk menyimpan layanan baru
+Route::post('/services', [ServiceController::class, 'store'])->name('services.store');
+// Rute untuk menampilkan form edit layanan
+Route::get('/services/{service}/edit', [ServiceController::class, 'edit'])->name('services.edit');
+// Rute untuk memperbarui layanan
+Route::put('/services/{service}', [ServiceController::class, 'update'])->name('services.update');
+// Rute untuk menghapus layanan
+Route::delete('/services/{service}', [ServiceController::class, 'destroy'])->name('services.destroy');
+// Rute CRUD untuk kategori
+Route::resource('categories', CategoryController::class);
