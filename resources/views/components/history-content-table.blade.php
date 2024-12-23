fb0b322f51476795b96d01a539e0f231fc6df327
ryynaufall <rayyannaufal1112@gmail.com>
2024-12-23 18:17:36 +0700

    Update - Halaman Hitory 2

---
+++ resources/views/components/history-content-table.blade.php
@@ -0,0 +1,36 @@
+<div class="overflow-x-auto bg-white rounded-lg shadow-lg">
+    <table class="w-full min-w-[700px]">
+        <thead class="bg-red-900 text-white">
+            <tr>
+              <th class="py-3 px-4 text-center">ID</th>
+              <th class="py-3 px-4 text-center">Date</th>
+              <th class="py-3 px-4 text-center">Link</th>
+              <th class="py-3 px-4 text-center">Charge</th>
+              <th class="py-3 px-4 text-center">Start Count</th>
+              <th class="py-3 px-4 text-center">Quantity</th>
+              <th class="py-3 px-4 text-center">Service</th>
+              <th class="py-3 px-4 text-center">Status</th>
+              <th class="py-3 px-4 text-center">Remains</th>
+              <th class="py-3 px-4 text-center">Order Again</th>
+            </tr>
+        </thead>
+        <tbody>
+            <tr>
+              <td class="py-3 px-4 text-center">001</td>
+              <td class="py-3 px-4 text-center">2024-12-17</td>
+              <td class="py-3 px-4 text-center">
+                <a href="#" class="text-blue-500 underline">example.com</a>
+              </td>
+              <td class="py-3 px-4 text-center">$10.00</td>
+              <td class="py-3 px-4 text-center">100</td>
+              <td class="py-3 px-4 text-center">200</td>
+              <td class="py-3 px-4 text-center">SEO Service</td>
+              <td class="py-3 px-4 text-center">Completed</td>
+              <td class="py-3 px-4 text-center">0</td>
+              <td class="py-3 px-4 text-center">
+                <button class="bg-red-900 text-white py-1 px-3 rounded">Order</button>
+              </td>
+            </tr>
+        </tbody>
+    </table>
+</div>
