186015162993759876644924cfea1e2b477be04e
duls3345 <azis.rohman33@gmail.com>
2025-01-10 17:28:06 +0700

    update fiks

--- resources/views/user/history.blade.php
+++ resources/views/user/history.blade.php
@@ -16,8 +16,7 @@
 
       <!-- Main Content -->
       <main id="main-content" class="flex-1 lg:ml-64 p-4 lg:p-8 transition-all duration-300 ease-in-out">
-          <!-- Search Bar -->
-          <x-history-content-searchbar></x-history-content-searchbar>
+
 
           <!-- Scrollable Table -->
           <div id="ordersTable">
@@ -26,53 +25,5 @@
       </main>
   </div>
 
-  <!-- Script untuk AJAX Search -->
-  <script>
-      $(document).ready(function() {
-          function fetchOrders(page = 1) {
-              let query = $('#search').val();
 
-              $.ajax({
-                  url: "{{ route('user.history') }}",
-                  type: "GET",
-                  data: {
-                      search: query,
-                      page: page
-                  },
-                  success: function(response) {
-                      $('#ordersTable').html(response.html);
-                  },
-                  error: function(xhr) {
-                      console.log(xhr.responseText);
-                  }
-              });
-          }
-
-          // Live search dengan keyup (debounced)
-          let typingTimer;
-          let doneTypingInterval = 500; // waktu dalam milidetik
-
-          $('#search').on('keyup', function() {
-              clearTimeout(typingTimer);
-              typingTimer = setTimeout(fetchOrders, doneTypingInterval);
-          });
-
-          // Jika user masih mengetik, reset timer
-          $('#search').on('keydown', function() {
-              clearTimeout(typingTimer);
-          });
-
-          // Search button click
-          $('#searchButton').on('click', function() {
-              fetchOrders();
-          });
-
-          // Handle pagination link click
-          $(document).on('click', '#ordersTable .pagination a', function(e) {
-              e.preventDefault();
-              let page = $(this).attr('href').split('page=')[1];
-              fetchOrders(page);
-          });
-      });
-  </script>
 </x-user-layout>
